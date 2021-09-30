f9c11793bcb958df92f3abcf8519265d9b5e9da2
Selena <selena.poun@gmail.com>
2021-09-30 14:35:48 +0200

    update JS

--- home.js
+++ home.js
@@ -1,1 +1,20 @@
-let;
+let home = document.getElementById('home');
+let gamePage = document.getElementById('game-page');
+let buttonPlay = document.getElementById('play');
+
+buttonPlay.addEventListener("click", () => {
+    if(getComputedStyle(gamePage).display != "none"){
+        gamePage.style.display = "none";
+    } else {
+        game.style.display = "block";
+    }
+}
+
+function buttonPlay(){
+    if(getComputedStyle(gamePage)).display != "none"){
+      gamePage.style.display = "none";
+    } else {
+      gamePage.style.display = "block";
+    }
+  };
+  buttonPlay.onclick = gamePage;
